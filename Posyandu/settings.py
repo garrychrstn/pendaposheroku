29449f10ae47ea0a1e56d1677bc8c9d230852e94
Garry Christian <garrychrstn@gmail.com>
2023-07-06 10:40:08 +0700

    LAUNCH1.0: Heroku

--- Posyandu/settings.py
+++ Posyandu/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-92@9-=v4g9&@d%lw9z@80o*9b#y3j$*7_@a8v3vb#rln_*tf-v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -47,6 +47,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- Posyandu/urls.py
+++ Posyandu/urls.py
@@ -14,10 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.views.static import serve
+from django.conf import settings
 from django.urls import path, include
+from django.conf.urls import url
 from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls'))
+    path('', include('main.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
